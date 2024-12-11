9b37b149de6836f7a2af52d47d1ec7f5998b07b8
Prachee Kanchan <pracheekanchan@gmail.com>
2024-12-11 09:32:40 +0530

    create_projcet

---
+++ karate_app/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget{
+  const MyApp({super.key});
+
+  Widget build(BuildContext context){
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child:Text("HelloWorld"),
+        ),
+      ),
+    );
+  }
+}
